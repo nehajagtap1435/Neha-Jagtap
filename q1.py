20b010c2e8668d53db0d5ef3b4cf01da4418f239
nehajagtap1435 <59467184+nehajagtap1435@users.noreply.github.com>
2020-09-10 14:53:47 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,28 @@
+# q1 Display the given array in reverse order
+# taking array of size n as a input from user
+
+
+array = []
+n = int(input("enter no of elements: "))
+
+for i in range(0, n):
+    ele = input("element: ")
+    array.append(ele)
+
+
+# by using  reverse() method in python
+# array.reverse()
+# print('reversed_array: ', array)
+
+
+# by using slicing
+# reversed_array = array[::-1]
+# print('reversed_array: ', reversed_array)
+
+# using for loop
+n = len(array)
+reversed_array = []
+for i in range(0, n):
+    reversed_array.append(array[n-i-1])
+print('reversed_array: ', reversed_array)
+
